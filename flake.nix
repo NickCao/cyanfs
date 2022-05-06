bf2b4ed2769b4c4ef3aed4c03fd6f9d30a2e8378
Nick Cao <nickcao@nichi.co>
2022-05-06 15:09:15 +0800

    init project skeleton

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NickCao/nixpkgs/nixos-unstable-small";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = import nixpkgs { inherit system; }; in
+        {
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs;[ rustc cargo rustfmt rust-analyzer ];
+          };
+        }
+      );
+}
