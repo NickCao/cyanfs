2a813d6df31f78348e0bbe96e52c56e2572e2620
Nick Cao <nickcao@nichi.co>
2022-05-06 15:48:17 +0800

    import an example implementation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
-use fuser::mount2;
+use fuser::{mount2, MountOption};
 use sfs::SFS;
 
 fn main() {
-    mount2(SFS {}, "/tmp/sfs", &[]).unwrap();
+    let options = vec![
+        MountOption::FSName("sfs".to_string()),
+        MountOption::AutoUnmount,
+    ];
+    mount2(
+        SFS::new("/tmp/sfs-data".to_string(), false, true),
+        "/tmp/sfs".to_string(),
+        &options,
+    )
+    .unwrap();
 }
