101b0856d06e1aa5e3426cbf037444e6b032458c
Nick Cao <nickcao@nichi.co>
2022-05-06 22:18:02 +0800

    add block dev module

---
+++ src/block_dev.rs
@@ -0,0 +1,31 @@
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::io::Result;
+use std::os::unix::fs::OpenOptionsExt;
+use std::os::unix::prelude::FileExt;
+use std::path::Path;
+
+struct BlockDevice<const BLOCK_SIZE: usize> {
+    backing_file: File,
+}
+
+impl<const BLOCK_SIZE: usize> BlockDevice<BLOCK_SIZE> {
+    fn new<P: AsRef<Path>>(path: P) -> Result<Self> {
+        Ok(Self {
+            backing_file: OpenOptions::new()
+                .read(true)
+                .write(true)
+                .custom_flags(libc::O_DIRECT)
+                .custom_flags(libc::O_NOATIME)
+                .open(path)?,
+        })
+    }
+    fn read_block(&self, block_id: usize, buf: &mut [u8; BLOCK_SIZE]) -> Result<()> {
+        self.backing_file
+            .read_exact_at(buf, (block_id * BLOCK_SIZE) as u64)
+    }
+    fn write_block(&self, block_id: usize, buf: &[u8; BLOCK_SIZE]) -> Result<()> {
+        self.backing_file
+            .write_all_at(buf, (block_id * BLOCK_SIZE) as u64)
+    }
+}
