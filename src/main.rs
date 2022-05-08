07a05c8b971f8730594608a2d3cb419e37672531
Nick Cao <nickcao@nichi.co>
2022-05-08 14:40:54 +0800

    check default permissions

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,9 @@ use sfs::SFS;
 fn main() {
     let options = vec![
         MountOption::FSName("sfs".to_string()),
+        MountOption::AllowOther,
         MountOption::AutoUnmount,
+        MountOption::DefaultPermissions,
     ];
     mount2(
         SFS::new("/tmp/sfs-meta", "/tmp/sfs-data"),
