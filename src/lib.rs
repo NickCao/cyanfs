4d73af811c689fe25ec20e5c8fdd7f7b8c7c916a
Nick Cao <nickcao@nichi.co>
2022-05-08 15:49:39 +0800

    no copy file range

--- src/lib.rs
+++ src/lib.rs
@@ -714,19 +714,4 @@ impl Filesystem for SFS {
             reply.error(libc::EBADF);
         }
     }
-    fn copy_file_range(
-        &mut self,
-        _req: &Request<'_>,
-        _ino_in: u64,
-        _fh_in: u64,
-        _offset_in: i64,
-        _ino_out: u64,
-        _fh_out: u64,
-        _offset_out: i64,
-        _len: u64,
-        _flags: u32,
-        reply: fuser::ReplyWrite,
-    ) {
-        reply.error(libc::ENOSYS);
-    }
 }
